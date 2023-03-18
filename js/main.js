06dc2b006444a447165ad1a5f2227bec282c72eb
Max Lystopadov <senior.listik@gmail.com>
2023-03-18 13:41:58 +0200

    Changes

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const debt = document.querySelector('#debt');
+let minWage = 6700;
+const collectTypeInp = document.querySelectorAll('.collectType');
+const property = document.querySelector('#property');
+const currency = document.querySelector('#currency');
+const charge = document.querySelector('#charge');
+const formDebt = document.querySelector('.form__debt');
+const formDebtorType = document.querySelector('.form__debtorType');
+const company = document.querySelector('#company');
+const person = document.querySelector('#person');
+
+debt.addEventListener('input', calcDebt);
+
+function calcDebt() {
+    collectTypeInp.forEach((el) => {
+        if (el.checked == true && el.id == "property") {
+            if ((debt.value * 0.02) <= 10 * minWage) {
+                charge.innerText = (debt.value * 0.02).toFixed(2);
+            } else if ((debt.value * 0.02) > 10 * minWage){
+                charge.innerText = 10 * minWage + 'грн';
+            }
+        }
+    })
+}
+
+currency.addEventListener('change', toggleForm);
+
+function toggleForm() {
+    formDebtorType.classList.toggle('hide');
+    formDebt.classList.toggle('hide');
+}
+
+company.addEventListener('change', function () {
+    charge.innerText = 2 * minWage + 'грн';
+})
+person.addEventListener('change', function () {
+    charge.innerText = minWage + 'грн';
+})
+
+property.addEventListener('change', toggleForm);
